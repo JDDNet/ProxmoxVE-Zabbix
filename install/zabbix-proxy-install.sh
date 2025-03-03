0c0cab7bfa35ee2db5ba55c3881d27f64155491b
JDDNet <76491408+JDDNet@users.noreply.github.com>
2025-03-03 16:52:30 +0000

    Add Zabbix Proxy Install

---
+++ install/zabbix-proxy-install.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: JDDNet
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: [SOURCE_URL]
+
+# Import Functions und Setup
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+# Installing Dependencies with the 3 core dependencies (curl;sudo;mc)
+msg_info "Installing Dependencies"
+$STD apt-get install -y \
+  curl \
+  sudo \
+  mc \
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Zabbix Proxy"
+cd /tmp
+wget -q https://repo.zabbix.com/zabbix/7.2/release/debian/pool/main/z/zabbix-release/zabbix-release_latest+debian12_all.deb
+$STD dpkg -i /tmp/zabbix-release_latest+debian12_all.deb
+$STD apt-get update
+$STD apt-get install -y zabbix-proxy-pgsql zabbix-sql-scripts
+$STD apt-get install -y zabbix-agent2 zabbix-agent2-plugin-postgresql
+msg_ok "Installed Zabbix Proxy"
+
+msg_info "Setting up PostgreSQL"
+$STD apt-get install -y postgresql
+DB_NAME=zabbix-proxy-db
+DB_USER=zabbix
+DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)
+$STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
+$STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET default_transaction_isolation TO 'read committed';"
+$STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET timezone TO 'UTC'"
+zcat /usr/share/zabbix/sql-scripts/postgresql/server.sql.gz | sudo -u $DB_USER psql $DB_NAME &>/dev/null
+sed -i "s/^DBName=.*/DBName=$DB_NAME/" /etc/zabbix/zabbix_proxy.conf
+sed -i "s/^DBUser=.*/DBUser=$DB_USER/" /etc/zabbix/zabbix_proxy.conf
+sed -i "s/^# DBPassword=.*/DBPassword=$DB_PASS/" /etc/zabbix/zabbix_proxy.conf
+echo "" >~/zabbix.creds
+echo "zabbix Database Credentials" >>~/zabbix.creds
+echo "" >>~/zabbix.creds
+echo -e "zabbix Database User: \e[32m$DB_USER\e[0m" >>~/zabbix.creds
+echo -e "zabbix Database Password: \e[32m$DB_PASS\e[0m" >>~/zabbix.creds
+echo -e "zabbix Database Name: \e[32m$DB_NAME\e[0m" >>~/zabbix.creds
+msg_ok "Set up PostgreSQL"
+
+msg_info "Setting up SNMP Trapper"
+$STD apt-get install -y net-snmp-utils net-snmp-perl net-snmp
+$STD curl -o /usr/bin/zabbix_trap_receiver.pl https://git.zabbix.com/projects/ZBX/repos/zabbix/raw/misc/snmptrap/zabbix_trap_receiver.pl
+$STD chmod +x /usr/bin/zabbix_trap_receiver.pl
+$STD mkdir /var/log/snmptrap
+sed -i "s|^\$SNMPTrapperFile.*|\$SNMPTrapperFile = '/var/log/snmptrap/snmptrap.log';/" /usr/bin/zabbix_trap_receiver.pl
+sed -i "s/^authCommunity.*/authCommunity execute public/" /etc/snmp/snmptrapd.conf
+sed -i "s/^perl.*/perl do "/usr/bin/zabbix_trap_receiver.pl";/" /etc/snmp/snmptrapd.conf
+cat > /etc/logrotate.d/snmptrap <<EOL
+/var/log/snmptrap/snmptrap.log {
+    weekly
+    rotate 12
+    compress
+    delaycompress
+    missingok
+    notifempty
+}
+EOL
+msg_ok "Set up SNMP Trapper"
+
+msg_info "Setting up Zabbix Proxy with SNMP Trapper"
+sed -i "s/^SNMPTrapperFile=.*/SNMPTrapperFile=/var/log/snmptrap/snmptrap.log/" /etc/zabbix/zabbix_proxy.conf
+sed -i "s/^StartSNMPTrapper=.*/StartSNMPTrapper=1/" /etc/zabbix/zabbix_proxy.conf
+msg_ok "Set up Zabbix Proxy with SNMP Trapper"
+
+msg_info "Starting Services"
+systemctl restart zabbix-agent2 zabbix-proxy snmptrapd
+systemctl enable -q --now zabbix-agent2 zabbix-proxy snmptrapd
+msg_ok "Started Services"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -rf /tmp/zabbix-release_latest+debian12_all.deb
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
